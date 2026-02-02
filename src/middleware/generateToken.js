7811b7c9bd92be929400c9ce9f101b6a040ac734
DevanshParashar165 <parashardevansh165@gmail.com>
2026-02-02 23:59:25 +0530

    debugging for vercel deployment

--- src/middleware/generateToken.js
+++ src/middleware/generateToken.js
@@ -12,8 +12,8 @@ const generateToken = (res, user, message) => {
   return res
     .cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      secure: true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000,
     })
     .status(200)
